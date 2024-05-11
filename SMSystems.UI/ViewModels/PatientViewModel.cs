abd5101193ef8ff399fc1e744ee48a46f104e4d2
Gabriel Moura <desenv@rdc.com.br>
2024-05-11 00:49:30 -0300

    v0.0.0.3 - Adicionado paginação na página ListPatients

--- SMSystems.UI/ViewModels/PatientViewModel.cs
+++ SMSystems.UI/ViewModels/PatientViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SMSystems.Domain.Entities;
 using System.ComponentModel.DataAnnotations;
 
 namespace SMSystems.UI.ViewModels
@@ -6,7 +7,7 @@ namespace SMSystems.UI.ViewModels
     public class PatientViewModel 
     {
       
-        public int ID { get; private set; }
+        public int ID { get; set; }
         [BindProperty]
         [Required(ErrorMessage = "Preencha o campo Nome.")]
         public string? Name { get; set; }
@@ -21,10 +22,13 @@ namespace SMSystems.UI.ViewModels
         [EmailAddress(ErrorMessage = "Email inválido.")]     
         public string? Email { get; set; }
         public bool Active { get; set; } = true;
+
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = false)]
         [BindProperty]
         public DateTime BirthDate { get; set; }
         public string? ICD { get; set; }
+
+    
     }
 }
