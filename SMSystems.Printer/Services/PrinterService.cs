43bfb1d24af1d78b5f04dc2acc7169162e8cfe19
Gabriel Moura <desenv@rdc.com.br>
2024-06-28 01:02:53 -0300

    Implementação inicial geração de PDF

---
+++ SMSystems.Printer/Services/PrinterService.cs
@@ -0,0 +1,37 @@
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using SMSystems.Printer.Interfaces;
+
+
+namespace SMSystems.Printer.Services
+{
+    public class PrinterService : IPrinter
+    {       
+        public byte[] PrintPDF()
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {              
+
+                PdfWriter writer = new PdfWriter(ms);
+
+                PdfDocument pdf = new PdfDocument(writer);
+
+                Document document = new Document(pdf);
+
+                Paragraph header = new Paragraph("TESTE IMPRESSÃO").SetTextAlignment(TextAlignment.CENTER);
+
+                document.Add(header);
+                document.Close();
+                byte[] result = ms.ToArray();
+
+                return result;
+
+            }
+            
+
+
+        }
+    }
+}
--- SMSystems.UI/Pages/Invoices/Details.cshtml.cs
+++ SMSystems.UI/Pages/Invoices/Details.cshtml.cs
@@ -1,43 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Threading.Tasks;
+using iText.Layout;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using SMSystems.Application.Interfaces;
+using SMSystems.Application.Services;
 using SMSystems.Data;
 using SMSystems.Domain.Entities;
+using SMSystems.Printer;
+using SMSystems.Printer.Interfaces;
+using SMSystems.Printer.Services;
 
 namespace SMSystems.UI.Pages.Invoices
 {
     public class DetailsModel : PageModel
     {
-        private readonly SMSystems.Data.SMSystemsDBContext _context;
+        private readonly IInvoiceService _invoiceService;
+        private readonly ISessionService _sessionService;
+        
 
-        public DetailsModel(SMSystems.Data.SMSystemsDBContext context)
+
+        public DetailsModel(IInvoiceService invoiceService, ISessionService sessionservice)
         {
-            _context = context;
+            _invoiceService = invoiceService;
+            _sessionService = sessionservice;
+            
         }
 
-      public Invoice Invoice { get; set; } = default!; 
+        public Invoice Invoice { get; set; } = default!;
+        public List<Session> Sessions { get; set; } = new List<Session>();
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        [BindProperty]
+        public List<DateTime> SessionDates { get; set; } = new List<DateTime>();
+
+        public async Task<IActionResult> OnGetAsync(int id)
         {
-            if (id == null || _context.Invoices == null)
+
+            if (id == 0)
             {
                 return NotFound();
             }
 
-            var invoice = await _context.Invoices.FirstOrDefaultAsync(m => m.ID == id);
+            var invoice = await _invoiceService.GetInvoiceByIdAsync(id);
             if (invoice == null)
             {
                 return NotFound();
             }
-            else 
-            {
-                Invoice = invoice;
-            }
+
+            List<Session> sessions = _sessionService.GetAllInvoiceSessions(id).ToList();
+
+            invoice.Sessions = sessions;
+
+            Sessions = sessions;
+
+            Invoice = invoice;
+
             return Page();
         }
+
+
+       public async Task<IActionResult> OnPostAsync()
+        {
+            PrinterService printerService = new PrinterService();
+
+           
+            // Sua lógica para gerar o PDF aqui
+            byte[] doc = printerService.PrintPDF();
+            // Exemplo: retornar um arquivo PDF
+
+            return File(doc, "application/pdf", "document.pdf");
+        }
+       
     }
 }
