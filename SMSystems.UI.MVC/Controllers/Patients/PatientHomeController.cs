2bc6ecbaa3afa92a19576a1bfe6e5887c1003a08
Gabriel Moura <desenv@rdc.com.br>
2024-07-24 21:38:46 -0300

    teste

---
+++ SMSystems.UI.MVC/Controllers/Patients/PatientHomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SMSystems.Application.Interfaces;
+using SMSystems.Data;
+using SMSystems.Domain.Entities;
+namespace SMSystems.UI.MVC.Controllers.Patients
+{
+    public class PatientHomeController : Controller
+    {
+        private readonly IPatientService _patientService;
+
+        public PatientHomeController(IPatientService patientService)
+        {
+            _patientService = patientService ?? throw new ArgumentNullException(nameof(patientService));
+        }
+
+        [BindProperty]
+        public IList<Patient> Patients { get; set; } = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? PatientName { get; set; }
+
+        public async Task<IActionResult>Index(string? searchString)
+        {
+            var patients = await _patientService.GetAll().ToListAsync();
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                patients = patients.Where(search => search.Name.Contains(SearchString)).ToList();
+            }
+
+            var viewModel = new PatientViewModel
+            {
+                Patients = patients,
+                SearchString = searchString
+            };
+
+            return View(viewModel);
+
+        }
+    }
+}
---
+++ SMSystems.UI.MVC/ViewModels/PatientViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SMSystems.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace SMSystems.UI.MVC.ViewModels
+{
+
+    public class PatientViewModel
+    {
+
+        public int ID { get; set; }
+        [BindProperty]
+        [Required(ErrorMessage = "Preencha o campo Nome.")]
+        public string? Name { get; set; }
+        [BindProperty]
+        [Required(ErrorMessage = "Preencha o campo CPF.")]
+        [StringLength(11, ErrorMessage = "O CPF não pode ter mais do que 11 caracteres.")]
+        public string? SocialNumber { get; set; }
+        [BindProperty]
+        [Required(ErrorMessage = "Preencha o campo Telefone.")]
+        public string? Phone { get; set; }
+        [BindProperty]
+        [EmailAddress(ErrorMessage = "Email inválido.")]
+        public string? Email { get; set; }
+        public bool Active { get; set; } = true;
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = false)]
+        [BindProperty]
+        public DateTime BirthDate { get; set; }
+        public string? ICD { get; set; }
+
+
+    }
+}
